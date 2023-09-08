c383b507052f2861bd6ea1a3a66396e737b5ea64
Alexander Cramb <alexander.cramb.94@gmail.com>
2023-09-09 00:26:20 +0100

    Refactor profileRegistry to saveData

--- src/main/kotlin/com/ac101m/redmon/RedmonClient.kt
+++ src/main/kotlin/com/ac101m/redmon/RedmonClient.kt
@@ -5,7 +5,6 @@ import com.ac101m.redmon.profile.v1.SaveDataV1
 import com.ac101m.redmon.utils.Config.Companion.PROFILE_REGISTRY_SAVE_PATH
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.util.math.MatrixStack
 import java.nio.file.Path
@@ -13,34 +12,27 @@ import kotlin.io.path.exists
 
 
 class RedmonClient : ClientModInitializer {
-    private lateinit var profileRegistry: SaveDataV1
+    private lateinit var saveData: SaveDataV1
     private var currentContext: String? = null
 
-    private fun loadProfileRegistry(path: Path) {
+    private fun loadSaveData(path: Path) {
         if (path.exists()) {
-            profileRegistry = SaveData.load(path)
+            saveData = SaveData.load(path)
         } else {
-            profileRegistry = SaveDataV1()
-            profileRegistry.save(path)
+            saveData = SaveDataV1()
+            saveData.save(path)
         }
     }
 
     private fun drawOutput(matrixStack: MatrixStack) {
         MinecraftClient.getInstance().textRenderer.drawWithShadow(
-            matrixStack, currentContext.toString(), 0f, 0f, 0xffffff
+            matrixStack, "test", 0f, 0f, 0xffffff
         )
     }
 
     override fun onInitializeClient() {
-        // Load the profile registry (if it exists)
-        loadProfileRegistry(PROFILE_REGISTRY_SAVE_PATH)
+        loadSaveData(PROFILE_REGISTRY_SAVE_PATH)
 
-        // When leaving a world, reset the context ID
-        ServerPlayConnectionEvents.DISCONNECT.register { networkHandler, minecraftServer ->
-            currentContext = null
-        }
-
-        // Register rendering code with hud render callback
         HudRenderCallback.EVENT.register { matrixStack, _ ->
             if (currentContext != null) {
                 drawOutput(matrixStack)
