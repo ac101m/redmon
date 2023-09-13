7dd23eb6fb286ad5b39dbbdd5af75bf96f942438
Alexander Cramb <alexander.cramb.94@gmail.com>
2023-09-13 02:07:42 +0100

    Fix another erroneous rename

--- src/main/kotlin/com/ac101m/redmon/persistence/v1/PersistentProfileListV1.kt
+++ src/main/kotlin/com/ac101m/redmon/persistence/v1/PersistentProfileListV1.kt
@@ -8,7 +8,7 @@ import java.nio.file.Path
 import kotlin.io.path.outputStream
 
 
-data class PersistentPersistentProfileListV1(
+data class PersistentProfileListV1(
     @JsonProperty("profiles", required = true)
     val profiles: List<PersistentProfileV1>
 ) : PersistentProfileList() {
