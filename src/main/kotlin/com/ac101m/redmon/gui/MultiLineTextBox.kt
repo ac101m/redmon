cb76318928cc70d659ab1ec686e7019563c35e18
Alexander Cramb <alexander.cramb.94@gmail.com>
2023-09-17 22:51:27 +0100

    Fix zero register crash

--- src/main/kotlin/com/ac101m/redmon/gui/MultiLineTextBox.kt
+++ src/main/kotlin/com/ac101m/redmon/gui/MultiLineTextBox.kt
@@ -8,7 +8,7 @@ import net.minecraft.util.math.Vec3i
 
 class MultiLineTextBox(var lines: MutableList<String> = ArrayList()) : Drawable2D {
     override val height get() = (lines.size * 10) + 2
-    override val width get() = lines.maxOf { textWidth(it) + 3 }
+    override val width get() = lines.maxOfOrNull { textWidth(it) + 3 } ?: 0
 
     override fun draw(matrixStack: MatrixStack, position: Vec3i) {
         var currentPosition = position.add(Vec3i(1, 1, 0))
