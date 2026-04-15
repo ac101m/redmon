9d86286fbd207b5185634af903bc728b56b6185b
Alexander Cramb <alexander.cramb.94@gmail.com>
2026-04-15 22:13:37 +0100

    Add wire signal strength signal type

--- src/main/kotlin/com/ac101m/redmon/profile/SignalType.kt
+++ src/main/kotlin/com/ac101m/redmon/profile/SignalType.kt
@@ -11,6 +11,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties
  * Represents different types of signal.
  *
  * @property maxBlocks The maximum number of blocks a signal of this type can contain.
+ * @property bitsPerBlock The number of bits carried by each block.
  * @property block The Minecraft block type associated with this signal type.
  */
 enum class SignalType(val maxBlocks: Int, val bitsPerBlock: Int) {
@@ -18,12 +19,16 @@ enum class SignalType(val maxBlocks: Int, val bitsPerBlock: Int) {
     REPEATER(maxBlocks = 64, bitsPerBlock = 1),
 
     @JsonProperty("DUST_BINARY")
-    DUST_BINARY(maxBlocks = 64, bitsPerBlock = 1);
+    DUST_BINARY(maxBlocks = 64, bitsPerBlock = 1),
+
+    @JsonProperty("DUST_SS")
+    DUST_SS(maxBlocks = 16, bitsPerBlock = 4);
 
     val block: Block get() {
         return when (this) {
             REPEATER -> Blocks.REPEATER
             DUST_BINARY -> Blocks.REDSTONE_WIRE
+            DUST_SS -> Blocks.REDSTONE_WIRE
         }
     }
 
@@ -39,6 +44,7 @@ enum class SignalType(val maxBlocks: Int, val bitsPerBlock: Int) {
         return when (this) {
             REPEATER -> getBitsRepeater(blockState)
             DUST_BINARY -> getBitsDustBinary(blockState)
+            DUST_SS -> getBitsDustSignalStrength(blockState)
         }
     }
 
@@ -47,7 +53,7 @@ enum class SignalType(val maxBlocks: Int, val bitsPerBlock: Int) {
             return try {
                 SignalType.valueOf(str.uppercase())
             } catch (e: IllegalArgumentException) {
-                val validTypesString = SignalFormat.entries.joinToString(", ") { it.name.lowercase() }
+                val validTypesString = SignalType.entries.joinToString(", ") { it.name.lowercase() }
                 throw RedmonException("Invalid signal type. Valid types are $validTypesString", e)
             }
         }
@@ -58,6 +64,10 @@ enum class SignalType(val maxBlocks: Int, val bitsPerBlock: Int) {
 
         private fun getBitsDustBinary(blockState: BlockState): ULong {
             return if (blockState.getValue(BlockStateProperties.POWER) > 0) 1UL else 0UL
+        }
+
+        private fun getBitsDustSignalStrength(blockState: BlockState): ULong {
+            return blockState.getValue(BlockStateProperties.POWER).toULong()
         }
     }
 }
