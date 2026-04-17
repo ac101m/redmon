72002d88a6782c90740dc167d720f9f6935d7437
Alexander Cramb <alexander.cramb.94@gmail.com>
2026-04-17 17:33:57 +0100

    Allow list and search to function without a page number

--- src/main/kotlin/com/ac101m/redmon/CommandManager.kt
+++ src/main/kotlin/com/ac101m/redmon/CommandManager.kt
@@ -39,48 +39,66 @@ class CommandManager(
     val redmon: RedmonState
 ) {
     private fun LiteralArgumentBuilder<FabricClientCommandSource>.allCommands(): LiteralArgumentBuilder<FabricClientCommandSource> {
-        return this.then(literal("hide").executes { _ -> hideCommand() })
+        return this
+            .miscCommands()
+            .profileCommands()
+            .signalCommands()
+    }
+
+    private fun LiteralArgumentBuilder<FabricClientCommandSource>.miscCommands(): LiteralArgumentBuilder<FabricClientCommandSource> {
+        return this
+            .then(literal("hide").executes { _ -> hideCommand() })
             .then(literal("show").executes { _ -> showCommand() })
-            .then(literal("profile")
-                .then(literal("list").then(int("page", 1)
-                    .executes { c -> profileListCommand(c) }))
-                .then(literal("search").then(str("query").then(int("page", 1)
-                    .executes { c -> profileSearchCommand(c) } )))
-                .then(literal("create").then(str("name")
-                    .executes { c -> profileCreateCommand(c) }))
-                .then(literal("delete").then(str("name")
-                    .executes { c -> profileDeleteCommand(c) }))
-                .then(literal("select").then(str("name")
-                    .executes { c -> profileSelectCommand(c) }))
-                .then(literal("deselect").then(str("name")
-                    .executes { c -> profileDeselectCommand(c) }))
-                .then(literal("rename").then(str("name").then(str("new-name")
-                    .executes { c -> profileRenameCommand(c) })))
+    }
+
+    private fun LiteralArgumentBuilder<FabricClientCommandSource>.profileCommands(): LiteralArgumentBuilder<FabricClientCommandSource> {
+        return this.then(literal("profile")
+            .then(literal("list")
+                .executes { c -> profileListCommandSinglePage(c) })
+            .then(literal("list").then(int("page", 1)
+                .executes { c -> profileListCommand(c) }))
+            .then(literal("search").then(str("query")
+                .executes { c -> profileSearchCommandSinglePage(c) }))
+            .then(literal("search").then(str("query").then(int("page", 1)
+                .executes { c -> profileSearchCommand(c) } )))
+            .then(literal("create").then(str("name")
+                .executes { c -> profileCreateCommand(c) }))
+            .then(literal("delete").then(str("name")
+                .executes { c -> profileDeleteCommand(c) }))
+            .then(literal("select").then(str("name")
+                .executes { c -> profileSelectCommand(c) }))
+            .then(literal("deselect").then(str("name")
+                .executes { c -> profileDeselectCommand(c) }))
+            .then(literal("rename").then(str("name").then(str("new-name")
+                .executes { c -> profileRenameCommand(c) })))
+        )
+    }
+
+    private fun LiteralArgumentBuilder<FabricClientCommandSource>.signalCommands(): LiteralArgumentBuilder<FabricClientCommandSource> {
+        return this.then(literal("signal")
+            .then(literal("add").then(str("name").then(str("type").then(int("block-count")
+                .executes { c -> signalAddCommand(c) }))))
+            .then(literal("delete").then(str("name")
+                .executes { c -> signalDeleteCommand(c) }))
+            .then(literal("invert").then(str("name")
+                .executes { c -> signalInvertCommand(c) }))
+            .then(literal("flip").then(str("name")
+                .executes { c -> signalFlipCommand(c) }))
+            .then(literal("add-block").then(str("name")
+                .executes { c -> signalAppendBlockCommand(c) }))
+            .then(literal("add-blocks").then(str("name").then(int("count", 0)
+                .executes { c -> signalAppendBlocksCommand(c) })))
+            .then(literal("rename").then(str("name").then(str("new-name")
+                .executes { c -> signalRenameCommand(c) })))
+            .then(literal("format").then(str("name").then(str("format")
+                .executes { c -> signalFormatCommand(c) })))
+            .then(literal("move").then(str("name")
+                .then(literal("up").then(int("count", 0)
+                    .executes { c -> signalMoveUpCommand(c) }))
+                .then(literal("down").then(int("count", 0)
+                    .executes { c -> signalMoveDownCommand(c) })))
             )
-            .then(literal("signal")
-                .then(literal("add").then(str("name").then(str("type").then(int("block-count")
-                    .executes { c -> signalAddCommand(c) }))))
-                .then(literal("delete").then(str("name")
-                    .executes { c -> signalDeleteCommand(c) }))
-                .then(literal("invert").then(str("name")
-                    .executes { c -> signalInvertCommand(c) }))
-                .then(literal("flip").then(str("name")
-                    .executes { c -> signalFlipCommand(c) }))
-                .then(literal("add-block").then(str("name")
-                    .executes { c -> signalAppendBlockCommand(c) }))
-                .then(literal("add-blocks").then(str("name").then(int("count", 0)
-                    .executes { c -> signalAppendBlocksCommand(c) })))
-                .then(literal("rename").then(str("name").then(str("new-name")
-                    .executes { c -> signalRenameCommand(c) })))
-                .then(literal("format").then(str("name").then(str("format")
-                    .executes { c -> signalFormatCommand(c) })))
-                .then(literal("move").then(str("name")
-                    .then(literal("up").then(int("count", 0)
-                        .executes { c -> signalMoveUpCommand(c) }))
-                    .then(literal("down").then(int("count", 0)
-                        .executes { c -> signalMoveDownCommand(c) })))
-                )
-            )
+        )
     }
 
     fun registerCommands(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
@@ -98,15 +116,13 @@ class CommandManager(
         return COMMAND_SUCCESS
     }
 
-    private fun paginateProfileList(ctx: CommandContext<FabricClientCommandSource>, names: List<String>) {
+    private fun paginateProfileList(ctx: CommandContext<FabricClientCommandSource>, names: List<String>, pageNumber: Int) {
         if (names.isEmpty()) {
             ctx.sendFeedback("No profiles found")
             return
         }
 
-        val pageNumber = getInteger(ctx, "page")
         val pageIndex = pageNumber - 1
-
         val pageCount = names.size.ceilDiv(PROFILES_PER_PAGE)
 
         require(pageIndex < pageCount) {
@@ -121,16 +137,32 @@ class CommandManager(
         ctx.sendFeedback("Found ${names.size} profiles (page $pageNumber/$pageCount):\n$list")
     }
 
-    private fun profileListCommand(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+    private fun doProfileList(ctx: CommandContext<FabricClientCommandSource>, pageNumber: Int) {
         val names = redmon.getProfileNames().sorted()
-        paginateProfileList(ctx, names)
+        paginateProfileList(ctx, names, pageNumber)
     }
 
-    private fun profileSearchCommand(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+    private fun doProfileSearch(ctx: CommandContext<FabricClientCommandSource>, pageNumber: Int) {
         val names = redmon.getProfileNames()
         val queryString = getString(ctx, "query")
         val filteredNames = names.filter { it.contains(queryString, ignoreCase = true) }
-        paginateProfileList(ctx, filteredNames)
+        paginateProfileList(ctx, filteredNames, pageNumber)
+    }
+
+    private fun profileListCommand(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+        doProfileList(ctx, getInteger(ctx, "page"))
+    }
+
+    private fun profileListCommandSinglePage(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+        doProfileList(ctx, 1)
+    }
+
+    private fun profileSearchCommand(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+        doProfileSearch(ctx, getInteger(ctx, "page"))
+    }
+
+    private fun profileSearchCommandSinglePage(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
+        doProfileSearch(ctx, 1)
     }
 
     private fun profileCreateCommand(ctx: CommandContext<FabricClientCommandSource>) = commandWrapper(ctx) {
